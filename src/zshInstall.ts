fc8aef029143c421a4fb978d3b13f3e2c74abcc6
Simon he <13917107460@163.com>
2022-09-14 22:02:37 +0800

    chore: update

--- src/zshInstall.ts
+++ src/zshInstall.ts
@@ -3,7 +3,8 @@ import { jsShell } from 'simon-js-tool'
 export function install() {
   const zsh_path = `${jsShell('echo $HOME')?.toString().trim()}/.oh-my-zsh/custom`
   jsShell([
-    'wget https://gitee.com/Jiepq/ohmyzsh/blob/master/tools/install.sh && sh install.sh', // 安装oh my zsh
+    'wget https://gitee.com/Jiepq/ohmyzsh/blob/master/tools/install.sh && sh install.sh ', // 安装oh my zsh
+    'rm - rf install.sh', // 删除安装脚本
     `git clone https://gitee.com/xiaoqqya/spaceship-prompt.git "${zsh_path}/themes/spaceship-prompt" --depth=1`, // 安装 spaceship-prompt
     `ln -s "${zsh_path}/themes/spaceship-prompt/spaceship.zsh-theme" "${zsh_path}/themes/spaceship.zsh-theme"`, // 创建软连接
     `git clone https://gitee.com/yanzhongqian/zsh-autosuggestions.git ${zsh_path}/plugins/zsh-autosuggestions`, // 安装 zsh-autosuggestions
